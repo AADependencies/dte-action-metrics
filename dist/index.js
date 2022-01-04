0533633a1982651410de64cbd5a8ce4a8c895382
Mohamed El-Malah <mohamed.elmalah@aa.com>
2022-01-04 12:06:11 -0600

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("@actions/core"));
-const actionName = core_1.default.getInput('action_name');
-console.log(`Hello from ${actionName}`);
+var core_1 = __importDefault(require("@actions/core"));
+var actionName = core_1.default.getInput('action_name');
+console.log("Hello from " + actionName);
