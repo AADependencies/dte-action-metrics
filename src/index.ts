5069e96cbf1f5e2d195e648cb19774a24736a3e6
Mohamed El-Malah <mohamed.elmalah@aa.com>
2022-01-04 14:43:53 -0600

    Print payload

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as core from '@actions/core';
-// import github from '@actions/github';
+import * as github from '@actions/github';
+
+const end_time = new Date().toTimeString();
+console.log(`Ended at ${end_time}`);
+core.setOutput('end_time', end_time);
 
 const actionName = core.getInput('action_name');
-
 console.log(`Hello from ${actionName}`);
+
+const payload = JSON.stringify(github.context.payload, undefined, 2);
+console.log(`The event payload: ${payload}`);
