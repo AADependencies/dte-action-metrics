64de5503e29371248b6352531fd92534693d455c
Bryan Stitt <bryan.stitt1@aa.com>
2022-01-18 15:05:07 -0600

    test

--- dist/index.js
+++ dist/index.js
@@ -101,7 +101,10 @@ function getActionVersion() {
                 },
             });
             const actionArray = response.data.split(" ");
-            actionArray.join('').split('');
+            const length = actionArray.length;
+            for (let i = 0; i < length; i++)
+                actionArray[i] && actionArray.push(actionArray[i]);
+            actionArray.splice(0, length);
             console.log("Action array: " + actionArray);
             // TODO: Parse the content to get the version ov action (can use action name to match file line)
             // Return the version
