19e913d02b78411ab9ad81be98ecac3e431848e0
Bryan Stitt <bryan.stitt1@aa.com>
2022-01-20 16:35:44 -0600

    test

--- dist/index.js
+++ dist/index.js
@@ -51,45 +51,30 @@ const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 const axios_1 = __importDefault(__nccwpck_require__(6545));
 const yaml_1 = __importDefault(__nccwpck_require__(3552));
-// interface ActionContext {
-//   action_name: string;
-//   actor: string;
-//   repo_name: string;
-//   action_version: Promise<string>;
-//   start_time: string;
-//   end_time: string;
-//   workflow_name: string;
-//   workflow_file: string;
-//   workflow_trigger: string;
-//   job_name: string;
-//   sha: string;
-//   repo_ref: string;
-//   run_url: string;
-// }
 const gh_token = process.env.GH_TOKEN;
-// const actionStartTime = core.getInput("start_time");
-// const actionEndTime = new Date().toTimeString();
+const actionStartTime = core.getInput("start_time");
+const actionEndTime = new Date().toTimeString();
 const actionName = core.getInput("action_name");
 const context = JSON.parse(JSON.stringify(github.context));
 // create var of type ActionContext
-// const actionContext: ActionContext = {
-//   action_name: actionName,
-//   actor: context.actor,
-// repo_name: context.payload.repository.name,
-//   action_version: getActionVersion(),
-//   start_time: actionStartTime,
-//   end_time: actionEndTime,
-//   workflow_name: context.workflow,
-//   workflow_file: context.payload.repository.workflow,
-//   workflow_trigger: context.eventName,
-//   job_name: context.job,
-//   sha: context.sha,
-//   repo_ref: context.ref,
-//   run_url: `${context.payload.repository.html_url}/actions/runs/${context.runId}`,
-// };
-getActionVersion();
+const actionContext = {
+    action_name: actionName,
+    actor: context.actor,
+    repo_name: context.payload.repository.name,
+    action_version: getActionVersion(),
+    start_time: actionStartTime,
+    end_time: actionEndTime,
+    workflow_name: context.workflow,
+    workflow_file: context.payload.repository.workflow,
+    workflow_trigger: context.eventName,
+    job_name: context.job,
+    sha: context.sha,
+    repo_ref: context.ref,
+    run_url: `${context.payload.repository.html_url}/actions/runs/${context.runId}`,
+};
+console.log(actionContext);
 function getActionVersion() {
-    var _a, _b;
+    var _a;
     return __awaiter(this, void 0, void 0, function* () {
         console.log("Getting action version");
         const wf_path = context.payload.workflow;
@@ -104,18 +89,14 @@ function getActionVersion() {
                 },
             });
             const doc = yaml_1.default.parseDocument(response.data);
-            // const {anchors, contents} = doc;
-            // const a = contents.items
-            console.log("Goal: " + ((_a = doc.contents) === null || _a === void 0 ? void 0 : _a.toJSON().jobs['sonarqube-monitor'].steps[1].with.ref));
-            const steps = (_b = doc.contents) === null || _b === void 0 ? void 0 : _b.toJSON().jobs[context.job].steps;
-            // const repo = "AAInternal/" + actionName;
+            const steps = (_a = doc.contents) === null || _a === void 0 ? void 0 : _a.toJSON().jobs[context.job].steps;
+            // const targetRepo = "AAInternal/" + actionName;
             const targetRepo = "AAInternal/sonarscan";
             for (const step of Object.keys(steps)) {
                 const stepWith = steps[step].hasOwnProperty('with') ? steps[step].with : null;
                 if (stepWith !== null) {
                     const stepRepo = stepWith.hasOwnProperty('repository') ? stepWith.repository : null;
-                    if (stepRepo === targetRepo) {
-                        console.log("Target: " + stepWith.ref);
+                    if (stepRepo === targetRepo && stepWith.hasOwnProperty('ref')) {
                         return stepWith.ref;
                     }
                 }
@@ -123,43 +104,13 @@ function getActionVersion() {
                     continue;
                 }
             }
-            return "REF NOT FOUND";
-            // const actionArray: string[] = response.data.split(" ");
-            // const ref: string = getRef(actionArray);
-            // console.log("Ref: " + ref);
-            // return ref;
-            // TODO: Parse the content to get the version ov action (can use action name to match file line)
-            // Return the version
-            // return response.body;
+            return "VERSION NOT FOUND";
         }
         catch (error) {
             console.log(error);
             return "Failed to get version";
         }
     });
-}
-function getRef(arr) {
-    const length = arr.length;
-    for (let i = 0; i < length; i++) {
-        arr[i] && arr.push(arr[i].replace(/(\r\n|\n|\r)/gm, ""));
-    }
-    arr.splice(0, length);
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 'AAInternal/sonarscan') {
-            let backward = i - 1, forward = (i + 1 < arr.length) ? i + 1 : i;
-            while (backward > 0 || forward < arr.length) {
-                if (backward > 0 && arr[backward] === 'ref:') {
-                    return arr[backward + 1];
-                }
-                if (forward < arr.length && arr[forward] === 'ref:') {
-                    return arr[forward + 1];
-                }
-                backward--;
-                forward++;
-            }
-        }
-    }
-    return "REF NOT FOUND";
 }
 // TO-DO - send to micro-service
 // Will need eventhub name and data in call
